fab2c7a547ea07003e840eaa5472537f97d398d2
Blinky <chirayurana@yahoo.com>
2024-03-09 17:54:45 +0545

    Created Navigation and Implemented Dark Mode

---
+++ app/src/main/java/com/chirayu/financeapp/MainActivity.kt
@@ -0,0 +1,154 @@
+package com.chirayu.financeapp
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.chirayu.financeapp.ui.theme.FinanceAppTheme
+
+class MainActivity : ComponentActivity() {
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+        FinanceAppTheme {
+            val navController = rememberNavController()
+            val backStackEntry = navController.currentBackStackEntryAsState()
+            Scaffold(
+                bottomBar = {
+                        NavigationBar {
+                            NavigationBarItem(
+                                selected = backStackEntry.value?.destination?.route == "expenses",
+                                onClick = { navController.navigate("expenses") },
+                                label = {
+                                    Text("Expenses")
+                                },
+                                icon = {
+                                    Icon(
+                                        painterResource(id = R.drawable.baseline_upload_24),
+                                        contentDescription = "Upload"
+                                    )
+                                }
+                            )
+                            NavigationBarItem(
+                                selected = backStackEntry.value?.destination?.route == "reports",
+                                onClick = { navController.navigate("reports") },
+                                label = {
+                                    Text("Reports")
+                                },
+                                icon = {
+                                    Icon(
+                                        painterResource(id = R.drawable.bar_chart),
+                                        contentDescription = "Bar Chart"
+                                    )
+                                }
+                            )
+                            NavigationBarItem(
+                                selected = backStackEntry.value?.destination?.route == "add",
+                                onClick = { navController.navigate("add") },
+                                label = {
+                                    Text("Add")
+                                },
+                                icon = {
+                                    Icon(
+                                        painterResource(id = R.drawable.add),
+                                        contentDescription = "Add"
+                                    )
+                                }
+                            )
+                            NavigationBarItem(
+                                selected = backStackEntry.value?.destination?.route == "settings",
+                                onClick = { navController.navigate("settings") },
+                                label = {
+                                    Text("Settings")
+                                },
+                                icon = {
+                                    Icon(
+                                        painterResource(id = R.drawable.settings),
+                                        contentDescription = "Settings"
+                                    )
+                                }
+                            )
+                        }
+                    }
+                ,
+                content = { innerPadding ->
+                    NavHost(navController = navController, startDestination = "expenses"){
+                        composable("expenses"){
+                            Surface(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(innerPadding)
+                            ) {
+                                Greeting(name = "Expenses")
+                            }
+                        }
+                        composable("reports"){
+                            Surface(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(innerPadding)
+                            ) {
+                                Greeting(name = "Reports")
+                            }
+                        }
+                        composable("add"){
+                            Surface(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(innerPadding)
+                            ) {
+                                Greeting(name = "Add")
+                            }
+                        }
+                        composable("settings"){
+                            Surface(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(innerPadding)
+                            ) {
+                                Greeting(name = "Settings")
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
+
+}
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+    @Composable
+    fun GreetingPreview() {
+        FinanceAppTheme {
+            Surface {
+                Greeting("Android")
+            }
+        }
+    }
+
---
+++ app/src/main/java/com/chirayu/financeapp/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.chirayu.financeapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Surface = Color.Black
+val TextPrimary = Color.White
+val Destructive = Color(0xFFFF453A)
+val Primary = Color(0xFF0A84FF)
+
+var SystemGray = Color(0xFF3A3A3C)
+var BackgroundElevated = Color(0xFF1C1C1E)
+var LabelSecondary = Color(0xFF8E8E93)
